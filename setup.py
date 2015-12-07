bee82dc7b91adf19136f236072404f6e1f3ef0ea
Shumon Huque <shuque@gmail.com>
2015-12-07 15:58:34 -0500

    version 1.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pydig',
-      version='1.00',
+      version='1.1.0',
       scripts=['pydig'],
       description='DNS query tool',
       author='Shumon Huque',
