5585d7a4791934f8490a2312d93e17ef2548822b
Shumon Huque <shuque@gmail.com>
2018-04-29 16:05:45 -0400

    Re-organize Opt RR code.

--- pydiglib/query.py
+++ pydiglib/query.py
@@ -16,102 +16,121 @@ def mk_id():
         return random.randint(1,65535)
 
 
-def mk_option_nsid():
-    """Construct EDNS NSID option"""
-    optcode = struct.pack('!H', 3)
-    optlen = struct.pack('!H', 0)
-    return optcode + optlen
+class OptRR:
 
+    """EDNS OPT Resource Record; see RFC 2671 and 6891"""
+    version = 0
+    udpbufsize = EDNS0_UDPSIZE
+    flags = 0
+    dnssec_ok = False
+    ercode = 0
+    rrname = b'\x00'                                     # empty domain
+    rrtype = struct.pack('!H', qt.get_val("OPT"))        # OPT type code
+    rrclass = None
+    rdlen = 0
+    rdlen_packed = None
+    rdata = b""
 
-def mk_option_expire():
-    """Construct EDNS Expire option"""
-    optcode = struct.pack('!H', 9)
-    optlen = struct.pack('!H', 0)
-    return optcode + optlen
+    def __init__(self, version, udpbufsize, flags, dnssec_ok):
+        self.version = version
+        self.udpbufsize = udpbufsize
+        self.rrclass = struct.pack('!H', udpbufsize)
+        self.dnssec_ok = dnssec_ok
+        if flags != 0:
+            self.flags = flags
+        elif dnssec_ok:
+            self.flags = 0x8000
+        else:
+            self.flags = 0x0
+        return
 
+    def mk_option_nsid(self):
+        """Construct EDNS NSID option"""
+        optcode = struct.pack('!H', 3)
+        optlen = struct.pack('!H', 0)
+        return optcode + optlen
 
-def mk_option_client_subnet():
-    """construct EDNS client subnet option"""
-    prefix_addr, prefix_len = options["subnet"].split("/")
-    prefix_len = int(prefix_len)
-    addr_octets = int(math.ceil(prefix_len/8.0))
-    if prefix_addr.find('.') != -1:                    # IPv4
-        af = struct.pack('!H', 1)
-        address = socket.inet_pton(socket.AF_INET, prefix_addr)[0:addr_octets]
-    elif prefix_addr.find(':') != -1:                  # IPv6
-        af = struct.pack('!H', 2)
-        address = socket.inet_pton(socket.AF_INET6, prefix_addr)[0:addr_octets]
-    else:
-        raise ErrorMessage("Invalid client subnet address: %s" % prefix_addr)
-    src_prefix_len = struct.pack('B', prefix_len)
-    scope_prefix_len = b'\x00'
-    optcode = struct.pack('!H', 8)
-    optdata = af + src_prefix_len + scope_prefix_len + address
-    optlen = struct.pack('!H', len(optdata))
-    return optcode + optlen + optdata
+    def mk_option_expire(self):
+        """Construct EDNS Expire option"""
+        optcode = struct.pack('!H', 9)
+        optlen = struct.pack('!H', 0)
+        return optcode + optlen
 
-
-def mk_option_cookie():
-    """Construct EDNS cookie option"""
-    optcode = struct.pack('!H', 10)
-    if options["cookie"] == True:
-        optdata = os.urandom(8)
-        optlen = struct.pack('!H', 8)
-    else:
-        try:
-            optdata = h2bin(options["cookie"])
-        except:
-            raise ErrorMessage("Malformed cookie: %s" % options["cookie"])
+    def mk_option_client_subnet(self):
+        """construct EDNS client subnet option"""
+        prefix_addr, prefix_len = options["subnet"].split("/")
+        prefix_len = int(prefix_len)
+        addr_octets = int(math.ceil(prefix_len/8.0))
+        if prefix_addr.find('.') != -1:                    # IPv4
+            af = struct.pack('!H', 1)
+            address = socket.inet_pton(socket.AF_INET,
+                                       prefix_addr)[0:addr_octets]
+        elif prefix_addr.find(':') != -1:                  # IPv6
+            af = struct.pack('!H', 2)
+            address = socket.inet_pton(socket.AF_INET6,
+                                       prefix_addr)[0:addr_octets]
+        else:
+            raise ErrorMessage("Invalid client subnet: %s" % prefix_addr)
+        src_prefix_len = struct.pack('B', prefix_len)
+        scope_prefix_len = b'\x00'
+        optcode = struct.pack('!H', 8)
+        optdata = af + src_prefix_len + scope_prefix_len + address
         optlen = struct.pack('!H', len(optdata))
-    return optcode + optlen + optdata
+        return optcode + optlen + optdata
 
+    def mk_option_cookie(self):
+        """Construct EDNS cookie option"""
+        optcode = struct.pack('!H', 10)
+        if options["cookie"] == True:
+            optdata = os.urandom(8)
+            optlen = struct.pack('!H', 8)
+        else:
+            try:
+                optdata = h2bin(options["cookie"])
+            except:
+                raise ErrorMessage("Malformed cookie: %s" % options["cookie"])
+            optlen = struct.pack('!H', len(optdata))
+        return optcode + optlen + optdata
 
-def mk_option_chainquery():
-    """Construct EDNS chain query option"""
-    optcode = struct.pack('!H', 13)
-    if options["chainquery"] == True:
-        optdata = b'\x00'
-    else:
-        optdata = txt2domainname(options["chainquery"])
-    optlen = struct.pack('!H', len(optdata))
-    return optcode + optlen + optdata
-
-
-def mk_option_generic():
-    """Construct generic EDNS options"""
-    alldata = ""
-    for (n, s) in options["ednsopt"]:
-        optcode = struct.pack('!H', n)
-        optdata = h2bin(s)
+    def mk_option_chainquery(self):
+        """Construct EDNS chain query option"""
+        optcode = struct.pack('!H', 13)
+        if options["chainquery"] == True:
+            optdata = b'\x00'
+        else:
+            optdata = txt2domainname(options["chainquery"])
         optlen = struct.pack('!H', len(optdata))
-        alldata += optcode + optlen + optdata
-    return alldata
+        return optcode + optlen + optdata
 
+    def mk_option_generic(self):
+        """Construct generic EDNS options"""
+        alldata = ""
+        for (n, s) in options["ednsopt"]:
+            optcode = struct.pack('!H', n)
+            optdata = h2bin(s)
+            optlen = struct.pack('!H', len(optdata))
+            alldata += optcode + optlen + optdata
+        return alldata
 
-def mk_optrr(edns_version, udp_payload, flags=0, dnssec_ok=False):
-    """Create EDNS0 OPT RR; see RFC 2671"""
-    rdata     = b""
-    rrname    = b'\x00'                                   # empty domain
-    rrtype    = struct.pack('!H', qt.get_val("OPT"))     # OPT type code
-    rrclass = struct.pack('!H', udp_payload)             # udp payload
-    if flags != 0: z = flags
-    elif dnssec_ok: z = 0x8000
-    else:         z = 0x0
-    ttl   = struct.pack('!BBH', 0, edns_version, z)      # ercode, ver, flags
-    if options['nsid']:
-        rdata += mk_option_nsid()
-    if options['expire']:
-        rdata += mk_option_expire()
-    if options["cookie"]:
-        rdata += mk_option_cookie()
-    if options["subnet"]:
-        rdata += mk_option_client_subnet()
-    if options["chainquery"]:
-        rdata += mk_option_chainquery()
-    if options["ednsopt"]:
-        rdata += mk_option_generic()
-    rdlen = struct.pack('!H', len(rdata))
-    return (rrname + rrtype + rrclass + ttl + rdlen + rdata)
+    def mk_optrr(self):
+        """Create EDNS0 OPT RR; see RFC 2671"""
+        ttl   = struct.pack('!BBH', self.ercode, self.version, self.flags)
+        if options['nsid']:
+            self.rdata += self.mk_option_nsid()
+        if options['expire']:
+            self.rdata += self.mk_option_expire()
+        if options["cookie"]:
+            self.rdata += self.mk_option_cookie()
+        if options["subnet"]:
+            self.rdata += self.mk_option_client_subnet()
+        if options["chainquery"]:
+            self.rdata += self.mk_option_chainquery()
+        if options["ednsopt"]:
+            self.rdata += self.mk_option_generic()
+        self.rdlen = len(self.rdata)
+        self.rdlen_packed = struct.pack('!H', self.rdlen)
+        return (self.rrname + self.rrtype + self.rrclass + ttl +
+                self.rdlen_packed + self.rdata)
 
 
 def mk_request(query, sent_id, options):
@@ -131,16 +150,6 @@ def mk_request(query, sent_id, options):
     ancount = struct.pack('!H', 0)              # 0 answer
     nscount = struct.pack('!H', 0)              # 0 authority
 
-    if options["use_edns"]:
-        arcount = struct.pack('!H', 1)
-        additional = mk_optrr(options["edns_version"],
-                              options["bufsize"],
-                              flags=options["edns_flags"],
-                              dnssec_ok=options["dnssec_ok"])
-    else:
-        arcount = struct.pack('!H', 0)
-        additional = b""
-
     flags = (qr << 15) + (opcode << 11) + (aa << 10) + (tc << 9) + \
             (rd << 8) + (ra << 7) + (z << 6) + (ad << 5) + (cd << 4) + rcode
     flags = struct.pack('!H', flags)
@@ -149,6 +158,17 @@ def mk_request(query, sent_id, options):
 
     question = wire_qname + struct.pack('!H', query.qtype) + struct.pack('!H', query.qclass)
         
+    if options["use_edns"]:
+        Opt = OptRR(options["edns_version"],
+                    options["bufsize"],
+                    flags=options["edns_flags"],
+                    dnssec_ok=options["dnssec_ok"])
+        arcount = struct.pack('!H', 1)
+        additional = Opt.mk_optrr()
+    else:
+        arcount = struct.pack('!H', 0)
+        additional = b""
+
     msg = packed_id + flags + qdcount + ancount + nscount + arcount + \
           question + additional
 
