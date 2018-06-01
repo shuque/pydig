844218d6c9a1107c44985b79320c2adcd79b3f56
Shumon Huque <shuque@gmail.com>
2018-06-01 00:29:44 -0400

    Check content length for HTTPS.

--- pydiglib/https.py
+++ pydiglib/https.py
@@ -11,6 +11,21 @@ else:
 
     HTTPS_TIMEOUT=5
 
+    def checkContentLength(r):
+        # requests library doesn't check content length!
+        # https://blog.petrzemek.net/2018/04/22/on-incomplete-http-reads-and-the-requests-library-in-python/
+        expected = r.headers.get('Content-Length')
+        if expected is not None:
+            actual = r.raw.tell()
+            expected = int(expected)
+            if actual < expected:
+                raise IOError(
+                    'incomplete read ({} bytes read, {} more expected)'.format(
+                        actual, expected - actual)
+                )
+            return
+
+
     def send_request_https(message, url):
         """Send request via HTTPS"""
 
@@ -20,8 +35,8 @@ else:
         }
         resp = requests.post(url, headers=headers, data=message,
                              timeout=HTTPS_TIMEOUT)
+        checkContentLength(resp)
         status_code = resp.status_code
-
         if status_code != 200:
             print("ERROR: HTTP Response Code: {}".format(status_code))
             print(resp.headers)
